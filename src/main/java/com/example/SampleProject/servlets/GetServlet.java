6ff35d8a3b39586f561b6fbb2397fc3977a006ee
dungla <anhdungxd21@gmail.com>
2022-05-11 08:45:23 +0700

    add new

--- src/main/java/com/example/SampleProject/servlets/GetServlet.java
+++ src/main/java/com/example/SampleProject/servlets/GetServlet.java
@@ -1,6 +1,10 @@
 package com.example.SampleProject.servlets;
 
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebInitParam;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -10,10 +14,18 @@ import java.io.PrintWriter;
 /**
  * @author dungla
  */
+@WebServlet(urlPatterns = "/getServlet",
+            initParams = @WebInitParam(name = "URL", value = "http://weatherservice.com"))
 public class GetServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletConfig config = getServletConfig();
+        String url = config.getInitParameter("URL");
+        System.out.println(url);
+
+        ServletContext context = getServletContext();
+        System.out.println(context.getInitParameter("dbURL"));
         String value = req.getParameter("name");
         String httpResponse = "<html><h3>Welcome to Servlet!</h3></html>";
         PrintWriter writer = resp.getWriter();
--- src/main/java/com/example/SampleProject/servlets/HomeServlet.java
+++ src/main/java/com/example/SampleProject/servlets/HomeServlet.java
@@ -1,18 +1,40 @@
 package com.example.SampleProject.servlets;
 
+import com.example.SampleProject.dao.DBConnection;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 /**
  * @author dungla
  */
 public class HomeServlet extends HttpServlet {
+    private Connection connection = null;
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("in doGet method");
         req.getRequestDispatcher("/html/index.html").forward(req, resp);
+    }
+
+    @Override
+    public void init() throws ServletException {
+        System.out.println("in init method");
+        connection = DBConnection.getConnectionToDatabase();
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("in destroy method");
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
